cf93ef9d69c5bf36ae70a74a32463e825ee8ed56
Hadhami-ghrs <hadhamigharsallah9@gmail.com>
2025-02-10 15:57:50 +0100

    core-module

--- front-mobile/src/app/app.module.ts
+++ front-mobile/src/app/app.module.ts
@@ -4,6 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { ClarityIcons, userIcon,boltIcon, cogIcon, homeIcon, certificateIcon } from '@cds/core/icon';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+ClarityIcons.addIcons(userIcon,boltIcon,cogIcon ,homeIcon,certificateIcon);
+
+import { ClarityModule } from '@clr/angular';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +18,8 @@ import { UserComponent } from './user/user.component';
     UserComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    BrowserModule,ClarityModule,CoreModule,
+    AppRoutingModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-mobile/src/app/core/core.module.ts
+++ front-mobile/src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClarityModule } from '@clr/angular';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent,SideNavComponent,FooterComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,ClarityModule,
+  ],
+  exports: [HeaderComponent,SideNavComponent,FooterComponent],
 })
 export class CoreModule { }
