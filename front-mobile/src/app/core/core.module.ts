94073165e7b1ccbb2d39832f009508996a1364a5
Hadhami-ghrs <hadhamigharsallah9@gmail.com>
2025-02-14 13:17:19 +0100

    coreModule

--- front-mobile/src/app/core/core.module.ts
+++ front-mobile/src/app/core/core.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
-import { FooterComponent } from './footer/footer.component';
 import { ClarityModule } from '@clr/angular';
 
 
 @NgModule({
-  declarations: [HeaderComponent,SideNavComponent,FooterComponent],
+  declarations: [HeaderComponent,SideNavComponent],
   imports: [
     CommonModule,ClarityModule,
   ],
-  exports: [HeaderComponent,SideNavComponent,FooterComponent],
+  exports: [HeaderComponent,SideNavComponent],
 })
 export class CoreModule { }
